c48b496916ab53776fe88e5d332e991a9517d5e1
DualNeXoN <samuelsmerek69@gmail.com>
2022-10-23 20:36:53 +0200

    Basic localization switching system
    Created basic localization switching system with examples.

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,17 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('language/{locale}', function ($locale) {
+    app()->setLocale($locale);
+    session()->put('locale', $locale);
+    return redirect()->back();
+});
+
+//Test translations
+Route::get('/translations', function () {
+    return view('translations');
+});
+
+Route::get('/translations/{param}', function ($param) {
+    return view('translations', compact('param'));
+});
+//End of: Test translations
