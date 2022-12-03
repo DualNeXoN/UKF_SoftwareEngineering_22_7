821a0f2ff8d0bd9bf0ea3e8b6e313bf897eb1b09
Nyusko <patrik.homa@icloud.com>
2022-12-03 18:56:41 +0100

    Added responsive navbar

--- src/resources/views/partials/navbar.blade.php
+++ src/resources/views/partials/navbar.blade.php
@@ -1,34 +1,39 @@
 <nav class="navbar navbar-expand-lg bg-nav">
     <div class="container-fluid">
         <a href="/"><img src="{{ asset('img/logo_pputils_t.png') }}" alt="Logo" width="75" class="navbar-brand"></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+
+        <button class="navbar-toggler me-auto" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar"">
             <span class="navbar-toggler-icon"></span>
         </button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
 
-        <div class="collapse navbar-collapse" id="mynavbar">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="navbar-brand nav-link mw mx-2" href="/">Home</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="navbar-brand nav-link dropdown-toggle mw mx-2" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Admin Panel
-                    </a>
-                    <div class="navbar-brand dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="navbar-brand dropdown-item" href="/admin/users">Users</a>
-                        <a class="navbar-brand dropdown-item" href="/admin/companies">Companies</a>
-                        <a class="navbar-brand dropdown-item" href="/admin/departments">Departments</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    @include('partials.languageSwitcher')
-                </li>
-            </ul>
+            <div class="offcanvas-body">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active mw mx-2" href="/">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link active dropdown-toggle mw mx-2" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Admin Panel
+                        </a>
+                        <div class="navbar-brand dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="navbar-brand dropdown-item" href="/admin/users">Users</a>
+                            <a class="navbar-brand dropdown-item" href="/admin/companies">Companies</a>
+                            <a class="navbar-brand dropdown-item" href="/admin/departments">Departments</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        @include('partials.languageSwitcher')
+                    </li>
+                </ul>
+            </div>
         </div>
-
         @if (Session::has('user'))
             <?php $user = Session::get('user')[0]; ?>
-            <a href="/students" class="h3">{{ $user["name"]. ' ' . $user["surname"] }}</a>
+            <a href="/students" class="h3">{{ $user['name'] . ' ' . $user['surname'] }}</a>
         @else
             <!-- Login form -->
             @if (Session::has('fail'))
@@ -36,10 +41,11 @@
             @elseif(Session::has('success'))
                 <div class="alert alert-success">{{ Session::get('success') }} </div>
             @endif
-            <form class="d-flex" method="POST" action="{{ route('login-user') }}">
+
+            <form class="d-flex ms-auto" method="POST" action="{{ route('login-user') }}">
                 @csrf
-                <div class="forms-inputs collapse navbar-collapse" id="mynavbar">
-                    <input type="text" name="uid" value="" placeholder="Ais ID" class="form-control-lg input-w" />
+                <div class="forms-inputs">
+                    <input type="text" name="uid" value="" placeholder="Ais ID" class="form-control input-w" />
                     @error('uid')
                         <span class="text-danger">
                             {{ $message }}}
@@ -47,16 +53,17 @@
                     @enderror
                 </div>
 
-                <div class="forms-inputs btwn-2 collapse navbar-collapse" id="mynavbar">
-                    <input type="password" name="password" id="password" placeholder="Password" class="form-control form-control-lg input-w" />
+                <div class="forms-inputs btwn-2">
+                    <input type="password" name="password" id="password" placeholder="Password" class="form-control input-w" />
                     <span class="text-danger">
                         @error('password')
                             {{ $message }}}
                         @enderror
                     </span>
                 </div>
-                <button class="btn-control btn btn-outline-dark btn-lg btwn-2 collapse navbar-collapse px-4" id="mynavbar" type="submit">Log in</button>
+                <button class="btn-control btn btn-outline-dark btwn-2" type="submit">Login</button>
             </form>
         @endif
     </div>
+
 </nav>
