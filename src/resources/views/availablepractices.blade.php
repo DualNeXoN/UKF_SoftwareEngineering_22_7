603be6e3afa8ec89f913267c8d856c039465d124
DualNeXoN <samuelsmerek69@gmail.com>
2022-12-05 15:40:02 +0100

    Refactored list of available prof. practices

--- src/resources/views/availablepractices.blade.php
+++ src/resources/views/availablepractices.blade.php
@@ -1,80 +1,68 @@
 @extends('layouts.layout')
 
 @section('head')
-    <link rel="stylesheet" href="{{asset("css/availablepractices.css")}}">
+    <link rel="stylesheet" href="{{ asset('css/availablepractices.css') }}">
 @endsection
 
 @section('content')
     <div class="container">
+
         <div class="row justify-content-center text-center">
             <div class="col main-label">
                 Professional Practice List
             </div>
         </div>
+
         <div class="row">
-            <div class="col">
-                <div class="table-responsive-md">
-                    <table class="table table-stripped">
-                        <thead>
-                            <tr>
-                                <th scope="col">Practice</th>
-                                <th scope="col">Company</th>
-                                <th scope="col">Contract</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-
-                            @if(count($practices) == 0)
-                                <td colspan="3" class="text-center"><strong>No professional practices recorded</strong></td>
-                            @else
-                            @foreach($practices as $practice)
-
-                                <tr>
-                                    <td scope="row" href="">{{ $practice->label }}</td>
-                                    <td >{{ $practice->company()->name }}</td>
-                                    <td>{{ $practice->contractType()->contract }}</td>
-                                    <td>
-                                        <div class="row">
-                                            @if($practice->isAvailable())
-                                            <div class="col-12 col-md-6 col-xl-4 operation">
-                                                <form method="POST" action="{{ route('practice-assign-student') }}">
-                                                    @csrf
-                                                    <input type="hidden" name="practiceId" value="{{ $practice->id }}"/>
-                                                    @if(Session::has('user'))
-                                                    <input type="hidden" name="studentId" value="{{ Session::get('user')['id'] }}"/>
-                                                    @else
-                                                    <input type="hidden" name="studentId" value=""/>
-                                                    @endif
-                                                    <button type="submit" class="btn btn-primary full-width">Assign to me</button>
-                                                </form>
-                                            </div>
-                                            @elseif(Session::has('user') && $practice->isUserAssigned(Session::get('user')['id']))
-                                            <div class="col-12 col-md-6 col-xl-4 operation">
-                                                <button type="submit" class="btn btn-success full-width" disabled>Assigned</button>
-                                            </div>
-                                            @else
-                                            <div class="col-12 col-md-6 col-xl-4 operation">
-                                                <button type="submit" class="btn btn-danger full-width" disabled>Full</button>
-                                            </div>
-                                            @endif
-                                            <div class="col-12 col-md-6 col-xl-4 operation">
-                                                <form method="GET" action="{{ route('practice-profile', [$practice->id]) }}">
-                                                    <button type="submit" class="btn btn-primary full-width">Details</button>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </td>
-                                </tr>
-
-                            @endforeach
-                            @endif
-
-                        </tbody>
-                    </table>
+            @if(count($practices) == 0)
+                <div class="col-12 text-center">
+                    <p><strong>No professional practices in database</strong></p>
                 </div>
-            </div>
+            @else
+            @foreach($practices as $practice)
+                <div class="col-12 col-md-12 col-lg-6 my-2">
+                    <div class="card @if($practice->isAvailable()) border-primary @elseif(Session::has('user') && $practice->isUserAssigned(Session::get('user')['id'])) border-success @else border-danger @endif text-center">
+                        <div class="card-body">
+                            <p class="card-text practice-label"><strong>{{ $practice->label }}</strong></p>
+                            <hr>
+                            <p class="card-text"><strong>Company: </strong>{{ $practice->company()->name }}</p>
+                            <hr>
+                            <p class="card-text"><strong>Contract Type: </strong>{{ $practice->contractType()->contract }}</p>
+                            <hr>
+                            <p class="card-text"><strong>Description</strong></p>
+                            <p class="card-text scroll">{{ $practice->description }}</p>
+                            <hr>
+                            @if($practice->isAvailable())
+                                <form method="POST" action="{{ route('practice-assign-student') }}">
+                                    @csrf
+                                    <input type="hidden" name="practiceId" value="{{ $practice->id }}" />
+                                    @if (Session::has('user'))
+                                        <input type="hidden" name="studentId" value="{{ Session::get('user')['id'] }}" />
+                                    @else
+                                        <input type="hidden" name="studentId" value="" />
+                                    @endif
+                                    <button type="submit" class="btn btn-primary full-width">Assign to me</button>
+                                </form>
+                            @elseif(Session::has('user') && $practice->isUserAssigned(Session::get('user')['id']))
+                                <div class="col-12 operation my-2">
+                                    <button type="submit" class="btn btn-success full-width" disabled>Assigned</button>
+                                </div>
+                            @else
+                                <div class="col-12 operation my-2">
+                                    <button type="submit" class="btn btn-danger full-width" disabled>Full</button>
+                                </div>
+                            @endif
+                            <div class="col-12 operation my-2">
+                                <form method="GET" action="{{ route('practice-profile', [$practice->id]) }}">
+                                    <button type="submit" class="btn btn-primary full-width">Details</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+            @endif
         </div>
 
     </div>
-
 @endsection
