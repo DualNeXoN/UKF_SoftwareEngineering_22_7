37192bc5d7fbb8b3dccc84aaebd1743e494f9ba1
AVAKSon <artem.senkov2014@gmail.com>
2022-12-06 17:39:49 +0100

    first graph implementation

--- src/resources/views/graph.blade.php
+++ src/resources/views/graph.blade.php
@@ -5,5 +5,46 @@
 @endsection
 
 @section('content')
-    This is graph page
+
+
+    <div>
+        <canvas id="myChart"></canvas>
+    </div>
+    
+    <script>
+
+        $.getJSON('http://localhost:8080/graph/data', function(data) {
+            
+            var labels = data.academic_year.map(function(e) {
+                return e;
+            });
+            var data = data.number_of_students.map(function(e) {
+                return e;
+            });
+
+            const ctx = document.getElementById('myChart');
+        
+            new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: '# of Votes',
+                    data: data,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                y: {
+                    beginAtZero: true
+                }
+                }
+            }
+            });
+        });
+
+    </script>
+   
+
 @endsection
