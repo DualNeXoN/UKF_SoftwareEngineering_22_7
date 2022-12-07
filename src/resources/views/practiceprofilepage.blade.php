9849df286eb625c347703c38502eab564a475e65
Richard Vanek <vanek.richard2422@gmail.com>
2022-12-07 01:02:02 +0100

    add profesional practice list

--- src/resources/views/practiceprofilepage.blade.php
+++ src/resources/views/practiceprofilepage.blade.php
@@ -1,5 +1,71 @@
 @extends('layouts.layout')
 
 @section('content')
-    {{$practice}}
+   <!-- {{$practice}}-->
+   <div class="container z-depth-1 my-5 p-5">
+
+<!-- Section -->
+<section>
+  <h3 class="font-weight-bold text-center dark-grey-text pb-2">PROFESIONAL PRACTICE LIST</h3>
+  <hr class="w-header my-4">
+ 
+  <div class="row d-flex justify-content-center">
+
+    <div class="col-md-6 col-xl-4">
+      <h5 class="font-weight-normal">Company</h5>
+    </div>
+
+    <div class="col-md-6">
+      <p class="text-muted mb-5 pb-2">Nieco o Company.............</p>
+    </div>
+
+
+    <div class="col-md-6 col-xl-4">
+      <h5 class="font-weight-normal">Contract type</h5>
+      
+    </div>
+
+    <div class="col-md-6">
+      <p class="text-muted mb-5 pb-2">Nieco......................................</p>
+    </div>
+
+
+    <div class="col-md-6 col-xl-4">
+      <h5 class="font-weight-normal">Description</h5>
+    </div>
+
+    <div class="col-md-6">
+      <p class="text-muted mb-5 pb-2">Nieco...............................................................................................................</p>
+    </div>
+
+    <div class="col-md-6 col-xl-4">
+      <h5 class="font-weight-normal">Number</h5>
+    </div>
+
+    <div class="col-md-6">
+      <p class="text-muted mb-5 pb-2">+42198473291</p>
+    </div>
+
+    <div class="col-md-6 col-xl-4">
+      <h5 class="font-weight-normal">Adress</h5>
+    </div>
+
+    <div class="col-md-6">
+      <p class="text-muted mb-5 pb-2">Adresa.................</p>
+    </div>
+
+    <div class="col-md-6 col-xl-4">
+      <h5 class="font-weight-normal">Contact Person</h5>
+    </div>
+
+    <div class="col-md-6">
+      <p class="text-muted mb-5 pb-2">Contact person.........................</p>
+    </div>
+
+  </div>
+
+</section>
+<!-- Section -->
+
+</div>
 @endsection
