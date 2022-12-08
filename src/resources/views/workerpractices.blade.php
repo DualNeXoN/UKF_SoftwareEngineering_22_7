388ba5336e2764e228ba68c35ea4990f759f941e
MatutuUKF <matus4286@gmail.com>
2022-12-08 04:42:11 +0100

    Small changes in workPractices

--- src/resources/views/workerpractices.blade.php
+++ src/resources/views/workerpractices.blade.php
@@ -1,14 +1,10 @@
 @extends('layouts.layout')
 
-@section('head')
-    <link rel="stylesheet" href="{{ asset('css/workerpractices.css') }}">
-@endsection
-
 @section('content')
     <div class="container">
         <div class="row justify-content-center text-center">
             <div class="col main-label">
-                <h2>Worker Practices</h2>
+                <h2>Practices</h2>
             </div>
         </div>
         <div class="row">
@@ -26,36 +22,36 @@
 
                         <tbody>
 
-                            @foreach ($practices as $practice)
-                                @if (!is_null($practice->studentPractice()) && $practice->studentPractice()->archived == 1)
+                        @foreach($practices as $practice)
+                            @if(!is_null($practice->studentPractice()) && $practice->studentPractice()->archived == 1)
                                 @else
-                                    <tr>
-                                        <td>{{ $practice->label }}</td>
-                                        <td>{{ $practice->company()->name }}</td>
-                                        @if (is_null($practice->studentPractice()))
-                                            <td class="text-danger justify-content-center">Not assigned</td>
-                                            <td>
-                                                <div class="col-12 col-xl-4 operation">
-                                                    <form method="post" action="#">
-                                                        <button type="submit" class="btn btn-primary full-width disabled">Show</button>
-                                                    </form>
-                                                </div>
-                                            </td>
-                                        @else
-                                            <td class="text-success justify-content-center">Assigned</td>
-                                            <td>
-                                                <div class="col-12 col-xl-4 operation">
-                                                    <form method="post" action="#">
-                                                        <a href="/"><button type="submit" class="btn btn-primary full-width">Show</button></a>
-                                                    </form>
-                                                </div>
-                                            </td>
-                                        @endif
-                                    </tr>
+                            <tr>
+                                <td>{{$practice->label}}</td>
+                                <td>{{$practice->company()->name}}</td>
+                            @if(is_null($practice->studentPractice()))
+                                    <td class="text-danger justify-content-center">Not assigned</td>
+                                    <td>
+                                        <div class="col-12 col-xl-4 operation">
+                                            <form method="post" action="/">
+                                                <button type="submit" class="btn btn-primary full-width disabled">Show</button>
+                                            </form>
+                                        </div>
+                                    </td>
+                                @else
+                                <td class="text-success justify-content-center">Assigned</td>
+                                <td>
+                                    <div class="col-12 col-xl-4 operation">
+                                        <form method="GET" action="/worker/practices/show/{{$practice->id}}">
+
+                                            <button type="submit" class="btn btn-primary full-width">Show</button>
+                                        </form>
+                                    </div>
+                                </td>
                                 @endif
+                            </tr>
+                            @endif
                             @endforeach
                         </tbody>
-
                     </table>
                 </div>
             </div>
